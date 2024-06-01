60d06894d8d06cf030f14e526653be7ec78c4518
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-06-01 11:51:53 +0700

    update footer admin

--- src/components/section/FooterAdmin.jsx
+++ src/components/section/FooterAdmin.jsx
@@ -1,7 +1,7 @@
 export default function FooterAdmin() {
   return (
     <footer className="bg-primary mt-auto">
-      <div className="container text-center">
+      <div className="container text-center py-3 text-white">
         <p className="mb-0">Created By Namamu</p>
       </div>
     </footer>
